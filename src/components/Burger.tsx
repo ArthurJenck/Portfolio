cc7c524440b5062ee296c81f0334cb86ebc8eea4
Arthur Jenck <arthurjenckdev@gmail.com>
2024-11-20 13:04:59 +0100

    fix: improved navbar folder structure

---
+++ src/components/Burger.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import ImgLink from "./ImgLink"
+import "../styles/Burger.scss"
+
+const Burger = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <div className={isOpen ? "burger open" : "burger"}>
+            <div className="burger-toggle" onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span className="burger-background"></span>
+            </div>{" "}
+            <div className="burger-menu">
+                <ImgLink for="logo" />
+                <ul>
+                    <li>
+                        <a href="/" onClick={() => setIsOpen(!isOpen)}>
+                            Accueil
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#skills" onClick={() => setIsOpen(!isOpen)}>
+                            Compétences
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#projets" onClick={() => setIsOpen(!isOpen)}>
+                            Projets
+                        </a>
+                    </li>
+                    <li>
+                        <a href="about" onClick={() => setIsOpen(!isOpen)}>
+                            À propos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="contact" onClick={() => setIsOpen(!isOpen)}>
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+                <div className="socials" onClick={() => setIsOpen(!isOpen)}>
+                    <ImgLink for="linkedin" />
+                    <ImgLink for="github" />
+                    <ImgLink for="cv" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Burger
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,9 @@
 import "../styles/NavBar.scss"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import ImgLink from "./ImgLink"
+import Burger from "./Burger"
 
 const NavBar = () => {
-    const [isOpen, setIsOpen] = useState(false)
-
     const hideLogoMobile = () => {
         window.matchMedia("(max-width: 768px)").matches
             ? (window.onscroll = () => {
@@ -29,59 +28,7 @@ const NavBar = () => {
     return (
         <nav>
             <ImgLink for="logo" />
-            <div className={isOpen ? "burger open" : "burger"}>
-                <div
-                    className="burger-toggle"
-                    onClick={() => setIsOpen(!isOpen)}
-                >
-                    <span></span>
-                    <span className="burger-background"></span>
-                </div>{" "}
-                <div className="burger-menu">
-                    <ImgLink for="logo" />
-                    <ul>
-                        <li>
-                            <a href="/" onClick={() => setIsOpen(!isOpen)}>
-                                Accueil
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#skills"
-                                onClick={() => setIsOpen(!isOpen)}
-                            >
-                                Compétences
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#projets"
-                                onClick={() => setIsOpen(!isOpen)}
-                            >
-                                Projets
-                            </a>
-                        </li>
-                        <li>
-                            <a href="about" onClick={() => setIsOpen(!isOpen)}>
-                                À propos
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="contact"
-                                onClick={() => setIsOpen(!isOpen)}
-                            >
-                                Contact
-                            </a>
-                        </li>
-                    </ul>
-                    <div className="socials" onClick={() => setIsOpen(!isOpen)}>
-                        <ImgLink for="linkedin" />
-                        <ImgLink for="github" />
-                        <ImgLink for="cv" />
-                    </div>
-                </div>
-            </div>
+            <Burger />
         </nav>
     )
 }
