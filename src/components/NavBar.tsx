f5f5467cb1ef587f5ab89e1f3df5a5f20011f845
Arthur Jenck <arthurjenckdev@gmail.com>
2024-11-22 19:24:47 +0100

    fix: corrected disappear scroll level for mobile logo / added correct meta and twitter tags

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         window.matchMedia("(max-width: 768px)").matches
             ? (window.onscroll = () => {
                   const navLogoOpacity = document.querySelector(".logo-link")!
-                  scrollY > window.innerHeight / 5
+                  scrollY > 0
                       ? navLogoOpacity.setAttribute(
                             "style",
                             "opacity:0; transition: opacity 0.2s"
