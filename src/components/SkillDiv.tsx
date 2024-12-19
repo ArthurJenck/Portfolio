101692be08dd6d2ad782c4c452174cfdfed03fce
Arthur Jenck <arthurjenckdev@gmail.com>
2024-12-19 14:51:18 +0100

    fix: showed skill name

--- src/components/SkillDiv.tsx
+++ src/components/SkillDiv.tsx
@@ -10,7 +10,10 @@ const SkillDiv = ({ pic, name, desc }: SkillDivProps) => {
     return (
         <li className="skill-div">
             <img src={pic} alt={`Logo ${name}`} title={name} />
-            <p>{desc}</p>
+            <div className="skill-div__desc">
+                <h4>{name}</h4>
+                <p>{desc}</p>
+            </div>
         </li>
     )
 }
